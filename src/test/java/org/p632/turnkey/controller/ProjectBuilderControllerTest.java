f4d003d44c90ab424c8361b979fb0a23a478ea77
Vivek <vivek.patani@somaiya.edu>
2016-03-30 11:32:11 -0400

    Updated Test Cases

--- src/test/java/org/p632/turnkey/controller/ProjectBuilderControllerTest.java
+++ src/test/java/org/p632/turnkey/controller/ProjectBuilderControllerTest.java
@@ -26,6 +26,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -81,6 +82,8 @@ public class ProjectBuilderControllerTest {
 		mockMvc.perform(post("/projectbuilder/buildTemplate")
 				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON));
+				//.body(templateModel));
+//				.andReturn());
 	}
 	/**
 	 * @throws java.lang.Exception
