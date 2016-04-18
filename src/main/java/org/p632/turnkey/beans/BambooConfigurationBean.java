70ed324ea17c47974cbcb5606e05212eaa8d18d5
rohsurve <rohit.a.surve@capgemini.com>
2016-04-18 12:45:26 -0400

    Changed Bamboo configuration class to log script execution

--- src/main/java/org/p632/turnkey/beans/BambooConfigurationBean.java
+++ src/main/java/org/p632/turnkey/beans/BambooConfigurationBean.java
@@ -1,6 +1,8 @@
 package org.p632.turnkey.beans;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 
 import org.p632.turnkey.helpers.Constants;
 import org.slf4j.Logger;
@@ -20,11 +22,19 @@ public class BambooConfigurationBean {
 		serverPath = serverPath.replace(".", File.separator);
 		String[] buildArgs = { "/bin/bash",templateName, serverPath + Constants.BAMBOO_SCRIPT,
 				Constants.GIT_REPOSITORY};
-
+        Process process;
 		try {
 			ProcessBuilder processBuilder = new ProcessBuilder(buildArgs);
 			processBuilder.directory(new File(serverPath));
-			processBuilder.start();
+			process=processBuilder.start();
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String line;
+			logger.info("Reading shells script");
+			while ((line = br.readLine()) != null) {
+			    logger.info(line);
+			}
+			
 			logger.debug("Bamboo build completed succesfully");
 		} catch (Exception ex) {
 			logger.error("Error during Bamboo build configuration", ex);
